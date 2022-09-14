133680dbdbc14fe1b08e56aa8fabfd82163e2710
lenincaldeira <lenin.caldeira@gmail.com>
2022-09-14 09:45:19 -0300

    Adicionando camadas de controle

--- src/main/java/controller/CustomerController.java
+++ src/main/java/controller/CustomerController.java
@@ -2,19 +2,23 @@ package controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import services.CustomerService;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.namespace.QName;
 
 @RestController
 @RequestMapping
 public class CustomerController {
     @Autowired
     private CustomerService service;
+    private ResponseEntity<String> QName;
 
     @PostMapping
-    public ResponseEntity<String> saveCustomer(@RequestParam(name = "file", required = true))
+    public ResponseEntity<String> saveCustomer(@RequestParam(name = "file", required = true) MultipartFile file, @PathVariable("id") Integer id, HttpServletResponse response) throws Exception {
+        return QName;
+    }
 
 }
